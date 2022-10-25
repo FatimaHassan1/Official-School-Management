017104ad028f753a4c62e359eea8c97a588d0637
asadkhan-react <asadfreelancer1234@gmail.com>
2022-10-25 11:23:19 -0700

    new commit 1

--- client/src/ExecutiveRedirect.jsx
+++ client/src/ExecutiveRedirect.jsx
@@ -29,6 +29,14 @@ function ExecutiveRedirect() {
               <input type='radio' name='menu'id='comp4'/>
               <label for='comp4'>
                   <div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide "><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path><polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line></svg></div>
+                  <NavLink exact to='/allstationary' style={{textDecoration:'none',}}>Stationary</NavLink>
+              </label>
+            </li>
+
+            <li>
+              <input type='radio' name='menu'id='comp4'/>
+              <label for='comp4'>
+                  <div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide "><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path><polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line></svg></div>
                   <NavLink exact to='/purchase' style={{textDecoration:'none',}}>Purchase</NavLink>
               </label>
             </li>
