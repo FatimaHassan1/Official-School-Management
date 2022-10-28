c90831dfe4832de58f69a6e3ecbe7652c60eff58
AwaisKhanJoiya <71541110+AwaisKhanJoiya@users.noreply.github.com>
2022-10-29 02:29:11 +0500

    added redirects file

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import './singlepdf.css';
-import './requestForms/alluser.css';
-import './students/students.css';
-import './Stationary/stationary.css';
-import './purchase/purchase.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./singlepdf.css";
+import "./requestForms/alluser.css";
+import "./students/students.css";
+import "./Stationary/stationary.css";
+import "./purchase/purchase.css";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename='/'>
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
