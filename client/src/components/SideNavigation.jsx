c97e5ceabbac22316d4f946774fdc11ae6a32b3d
asadkhan-react <asadfreelancer1234@gmail.com>
2022-11-03 14:30:58 -0700

    frontend error fixed

--- client/src/components/SideNavigation.jsx
+++ client/src/components/SideNavigation.jsx
@@ -149,65 +149,11 @@ function SideNavigation() {
                 </label>
               </li>
 
-
-              {/* MyWebsite */}
-              <li>
-                <input type="radio" name="menu" id="comp4" />
-                <label for="comp4">
-                  <div>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="lucide "
-                    >
-                      <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path>
-                      <polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline>
-                      <line x1="12" y1="22.08" x2="12" y2="12"></line>
-                    </svg>
-                  </div>
-                  <NavLink exact to="/mywebsite" style={{ textDecoration: "none" }}> MyWebsite </NavLink>
-                </label>
-              </li>
-
             </>
           )}
 
           {user_role === "Executive" && (
             <>
-
-              {/* Page */} 
-              <li>
-                <input type="radio" name="menu" id="comp4" />
-                <label for="comp4">
-                  <div>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="lucide "
-                    >
-                      <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path>
-                      <polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline>
-                      <line x1="12" y1="22.08" x2="12" y2="12"></line>
-                    </svg>
-                  </div>
-                  <NavLink exact to="/allpage" style={{ textDecoration: "none" }}> Page </NavLink>
-                </label>
-              </li>
-
 
               {/* Classrooms */}
               <li>
@@ -315,37 +261,6 @@ function SideNavigation() {
                   </div>
                   <NavLink exact to="/home" style={{ textDecoration: "none" }}>
                     Dashboard
-                  </NavLink>
-                </label>
-              </li>
-
-              <li>
-                <input type="radio" name="menu" id="comp4" />
-                <label for="comp4">
-                  <div>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="lucide "
-                    >
-                      <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path>
-                      <polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline>
-                      <line x1="12" y1="22.08" x2="12" y2="12"></line>
-                    </svg>
-                  </div>
-                  <NavLink
-                    exact
-                    to="/allpage"
-                    style={{ textDecoration: "none" }}
-                  >
-                    Page
                   </NavLink>
                 </label>
               </li>
@@ -627,60 +542,6 @@ function SideNavigation() {
                   <NavLink exact to="/levels" style={{ textDecoration: "none" }}> Levels </NavLink>
                 </label>
               </li>
-
-
-              {/* MyWebsite */}
-              <li>
-                <input type="radio" name="menu" id="comp4" />
-                <label for="comp4">
-                  <div>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="lucide "
-                    >
-                      <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path>
-                      <polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline>
-                      <line x1="12" y1="22.08" x2="12" y2="12"></line>
-                    </svg>
-                  </div>
-                  <NavLink exact to="/mywebsite" style={{ textDecoration: "none" }}> MyWebsite </NavLink>
-                </label>
-              </li>
-
-              {/* Page */}
-              <li>
-                <input type="radio" name="menu" id="comp4" />
-                <label for="comp4">
-                  <div>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="lucide "
-                    >
-                      <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path>
-                      <polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline>
-                      <line x1="12" y1="22.08" x2="12" y2="12"></line>
-                    </svg>
-                  </div>
-                  <NavLink exact to="/allpage" style={{ textDecoration: "none" }}> Page </NavLink>
-                </label>
-              </li>
-
 
               {/* Purchase */}
               <li>
